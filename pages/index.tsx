a0da199118523177828ded94521425c404c5aec9
adarshgajbhare <adarshgajbhare@gmail.com>
2023-10-29 09:17:48 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <div></div>;
+}
